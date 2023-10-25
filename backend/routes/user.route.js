5a27f5a7862e9694e922037f8f6dd1dd1e970f99
Hadson Dias Moto <hadson.mota@mac.barbosa>
2023-10-25 12:25:22 -0300

    feat:adicionado rota veiculo

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -47,30 +47,4 @@ router.post('/signin',(req,res)=>{
     })
 })
 
-router.get("/veiculo/:placa", async (req, res) => {
-    const placa = req.params.placa;
-    const userId = req.user.id; // Suponhamos que o ID do usuário esteja no objeto req.user após a autenticação.
-
-    if (placa.length < 9 && placa.length > 5) {
-        db.promise()
-            .execute("SELECT placa, ajuizamento FROM veiculos WHERE placa = ? AND user_id = ?;", [
-                placa,
-                userId
-            ])
-            .then(([rows]) => {
-                if (rows[0]) {
-                    res.status(200).json({ "placa": rows[0].placa, "ajuizamento": rows[0].ajuizamento });
-                } else {
-                    res.status(404).json({ "message": "Placa não encontrada no sistema" });
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-                res.status(500).json({ "error": "Erro ao conectar com o banco" });
-            });
-    } else {
-        res.status(422).json({ "error": "Dados insuficientes ou inválidos" });
-    }
-});
-
 module.exports = router
