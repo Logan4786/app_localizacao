f0d971d4c4489e5ac844802060e3c662cb30e4c0
Hadson Dias Moto <hadson.mota@mac.barbosa>
2023-10-25 13:45:55 -0300

    feat:adicionado rota veiculo

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,6 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 //mongoose.connect("mongodb://localhost:27017/mydb",{ useNewUrlParser: true, useUnifiedTopology: true })
 
-
 //Configurar middlewares e roteadores
 app.use(express.json());
 app.use(cors())
