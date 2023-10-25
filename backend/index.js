a7b1d1c4655d41c631fe4d1bf4e45298ab593637
Hadson Dias Moto <hadson.mota@mac.barbosa>
2023-10-25 13:42:24 -0300

    feat:adicionado rota veiculo

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,15 @@ const app = express()
 const port = 3000 || process.env.PORT
 const cors = require('cors')
 const bodyParser = require('body-parser')
+require('dotenv').config();
+
 
 
 const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 //mongoose.connect("mongodb://localhost:27017/mydb",{ useNewUrlParser: true, useUnifiedTopology: true })
-mongodb+srv;//logantenchy:ns4rJEwUYvKIKhLK@cluster0.bnwcg0e.mongodb.net/teste
+
 
 //Configurar middlewares e roteadores
 app.use(express.json());
