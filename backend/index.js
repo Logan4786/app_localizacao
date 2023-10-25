dc99353352cd319e87036946dd7270b4e528cc27
Hadson Dias Moto <hadson.mota@mac.barbosa>
2023-10-25 11:39:40 -0300

    Rota veiculo

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,13 @@ const port = 3000 || process.env.PORT
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
-const veiculosRouter = require('./routes/veiculos');
-const authRouter = require('./routes/auth');
-//const mongoose = require('mongoose')
-//mongoose.connect("mongodb://localhost:27017/mydb",{ useNewUrlParser: true, useUnifiedTopology: true })
+
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost:27017/mydb",{ useNewUrlParser: true, useUnifiedTopology: true })
 //mongodb+srv://logantenchy:ns4rJEwUYvKIKhLK@cluster0.bnwcg0e.mongodb.net/teste
 
 //Configurar middlewares e roteadores
 app.use(express.json());
-app.use('./veiculos', veiculosRouter);
-app.use('/auth', authRouter);
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
